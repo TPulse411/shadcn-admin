10d4692fbc02283cd2e01da5ad66e0515c905f9d
Arthur | TPulse <backend@tradingpulse.de>
2025-05-22 20:09:53 +0200

    Fix PostCSS configuration in Vite

--- vite.config.ts
+++ vite.config.ts
@@ -9,4 +9,10 @@ export default defineConfig({
       '@': path.resolve(__dirname, './src'),
     },
   },
+  css: {
+    postcss: {
+      include: [/\.css$/],
+      exclude: [/\.js$/, /\.jsx$/, /\.ts$/, /\.tsx$/]
+    }
+  }
 })
